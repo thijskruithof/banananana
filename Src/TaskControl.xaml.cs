b5432d4b8d8b90ece24a904ed49d9bedd51b06b5
Thijs <thijskruithof@gmail.com>
2018-09-22 14:09:26 +0200

    Work in progress on implementing the most basic functionality.

---
+++ Src/TaskControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Banananana
+{
+    /// <summary>
+    /// Interaction logic for TaskControl.xaml
+    /// </summary>
+    public partial class TaskControl : UserControl
+    {
+        public TaskControl()
+        {
+            InitializeComponent();
+
+            //richTextBox.Document.Blocks.Clear();
+            //richTextBox.Document.Blocks.Add(new Paragraph(new Run("Text")));
+        }
+
+        private void richTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
