b6586d4e567c3f9d2b4890313c9d8cc4ae08e39a
Unknown <thijskruithof@gmail.com>
2019-02-11 13:27:18 +0100

    Added icon to assembly info and set the assembly version to 0.1. As we're still very much in alpha.

--- Src/Properties/AssemblyInfo.cs
+++ Src/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("Banananana")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Guerrilla Games")]
+[assembly: AssemblyCompany("Thijs Kruithof")]
 [assembly: AssemblyProduct("Banananana")]
-[assembly: AssemblyCopyright("Copyright © Guerrilla Games 2018")]
+[assembly: AssemblyCopyright("Copyright © Thijs Kruithof, 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
