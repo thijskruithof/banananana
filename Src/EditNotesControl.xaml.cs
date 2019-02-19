665c5917c745b9940227cd6d50e4c06d144005a6
Thijs Kruithof <thijskruithof@gmail.com>
2019-02-19 13:28:46 +0100

    Work in progress on adding a 'notes' editor panel to the side of the window

---
+++ Src/EditNotesControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Banananana
+{
+    /// <summary>
+    /// Interaction logic for EditNotesControl.xaml
+    /// </summary>
+    public partial class EditNotesControl : UserControl
+    {
+        public EditNotesControl()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            closeButton.Opacity = 1.0;
+        }
+
+        private void closeButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            closeButton.Opacity = 0.25;
+        }
+
+        private void closeButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Visibility = Visibility.Collapsed;
+            (Parent as Grid).ColumnDefinitions[2].Width = new GridLength(0);
+            (Parent as Grid).ColumnDefinitions[1].Width = new GridLength(0);
+        }
+    }
+}
