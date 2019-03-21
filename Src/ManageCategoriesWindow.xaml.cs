5b80c1c9b42885adb155a7e4b771fbee5ba1156e
Thijs Kruithof <thijskruithof@gmail.com>
2019-03-21 13:35:08 +0100

    Work in progress on adding category management.

--- Src/ManageCategoriesWindow.xaml.cs
+++ Src/ManageCategoriesWindow.xaml.cs
@@ -19,6 +19,14 @@ namespace Banananana
     /// </summary>
     public partial class ManageCategoriesWindow : Window
     {
+        private class CategoriesListItem
+        {
+            public string Color { get; set; }
+            public string Title { get; set; }
+
+            public Workspace.Category Category { get; set; }
+        }
+
         private Workspace mWorkspace;
 
         public ManageCategoriesWindow(Workspace inWorkspace)
@@ -26,6 +34,8 @@ namespace Banananana
             mWorkspace = inWorkspace;
 
             InitializeComponent();
+            RebuildListView();
+            UpdateSelectedItemFields();
         }
 
         private void addButton_Click(object sender, RoutedEventArgs e)
@@ -41,15 +51,100 @@ namespace Banananana
 
         private void RebuildListView()
         {
-            categoriesListView.Items.Clear();
-            // TODO: Populate listview
+            Workspace.Category selected_item = null;
 
-            //foreach (Workspace.Category category in mWorkspace.Categories)
-            //{
-            //    ListViewItem item = new ListViewItem();
-            //    item
-            //    categoriesListView.Items.Add()
-            //}
+            if (categoriesListView.SelectedItem != null)
+                selected_item = (categoriesListView.SelectedItem as CategoriesListItem).Category;
+
+            categoriesListView.Items.Clear();
+
+            foreach (Workspace.Category category in mWorkspace.Categories)
+            {
+                CategoriesListItem item = new CategoriesListItem();
+                item.Color = category.Color.ToString();
+                item.Title = category.Title;
+                item.Category = category;
+
+                categoriesListView.Items.Add(item);
+            }
+
+            if (selected_item != null)
+            {
+                int idx = mWorkspace.Categories.IndexOf(selected_item);
+                if (idx >= 0)
+                    categoriesListView.SelectedIndex = idx;
+            }
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want delete this category? Any task that's assigned to this category will be assigned to the None category.", "Delete category?", MessageBoxButton.YesNo) != MessageBoxResult.Yes)
+                return;
+
+            CategoriesListItem item = categoriesListView.SelectedItem as CategoriesListItem;
+
+            if (item != null)
+            {
+                int category_index = mWorkspace.Categories.IndexOf(item.Category);
+                if (category_index >= 0)
+                {
+                    // Reset all tasks that are assigned to this category
+                    foreach (Workspace.Pile pile in mWorkspace.Piles)
+                        foreach (Workspace.Task task in pile.Tasks)
+                            if (task.CategoryIndex == category_index)
+                                task.CategoryIndex = -1;
+
+                    mWorkspace.Categories.RemoveAt(category_index);
+                    RebuildListView();
+                }
+            }            
+        }
+
+        private void categoriesListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateSelectedItemFields();
+        }
+
+        private void UpdateSelectedItemFields()
+        {
+            CategoriesListItem item = categoriesListView.SelectedItem as CategoriesListItem;
+
+            if (item != null)
+            {
+                titleTextBox.IsEnabled = true;
+                colorRect.Fill = new SolidColorBrush(item.Category.Color);
+                titleTextBox.Text = item.Title;
+                modifyButton.IsEnabled = true;
+            }
+            else
+            {
+                titleTextBox.IsEnabled = false;
+                titleTextBox.Text = "";
+                modifyButton.IsEnabled = false;
+            }
+        }
+
+        private void modifyButton_Click(object sender, RoutedEventArgs e)
+        {
+            CategoriesListItem item = categoriesListView.SelectedItem as CategoriesListItem;
+
+            if (item != null)
+            {
+                item.Category.Title = titleTextBox.Text;
+                item.Category.Color = (colorRect.Fill as SolidColorBrush).Color;
+                RebuildListView();
+            }
+        }
+
+        private void pickColorRect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            CategoriesListItem item = categoriesListView.SelectedItem as CategoriesListItem;
+
+            if (item != null)
+            {
+                item.Category.Color = ((sender as Rectangle).Fill as SolidColorBrush).Color;
+                colorRect.Fill = new SolidColorBrush(item.Category.Color);
+            }
         }
     }
 }
